2c78822eaac86415180404ddc2d81f5e4b5645f4
Vladislav Fatikhov <90382910+fatikhoov@users.noreply.github.com>
2025-02-02 23:02:49 +0300

    Update index.js
    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import CardList from 'cards-animated/src/components/CardList.js';
-
+ 
 document.addEventListener('DOMContentLoaded', () => {
 console.log('DOM готов к работе')
   const container = document.getElementById('blocks-container');
