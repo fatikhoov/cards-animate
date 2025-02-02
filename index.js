e9e0d410d65adfb85811905974e304f36178e3f8
Vladislav Fatikhov <90382910+fatikhoov@users.noreply.github.com>
2025-02-02 22:57:53 +0300

    update 7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
-import CardList from 'src/components/CardList.js';
+import CardList from './src/components/CardList.js';
 =======
-import CardList from 'src/components/CardList.js';
+import CardList from './src/components/CardList.js';
 >>>>>>> 6aec4b0dd12902fc0708822a786b1bfecbeb1915
 
 document.addEventListener('DOMContentLoaded', () => {
